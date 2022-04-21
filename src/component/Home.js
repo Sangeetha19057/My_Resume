cdef30524ac414ccaa8b68722667a24889ec28e4
Sangeetha <89621488+Sangeetha19057@users.noreply.github.com>
2022-04-21 17:32:42 +0300

    Update Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -42,7 +42,7 @@ export function Home() {
             Contact Me
           </a>
           <a
-            href="https://drive.google.com/file/d/1QnYXIcL4JmuUTnpFTJqVjLYMcARLfivy/view"
+            href="#"
             target="_blank"
             className="btn"
             rel="noopener noreferrer"
